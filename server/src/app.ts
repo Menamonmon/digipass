26c407ec704519b062e320032d00a72ded60f17c
menamonmon <monyasser111@gmail.com>
2022-04-18 22:30:42 -0400

    created a biolerplate app with type-graphql, express, and apollo serve

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,11 +1,34 @@
+import { ApolloServerPluginLandingPageGraphQLPlayground } from "apollo-server-core/dist/plugin/landingPage/graphqlPlayground";
+import { ApolloServer } from "apollo-server-express";
 import express from "express";
-const app = express();
-const port = 3000;
+import "reflect-metadata";
+import { buildSchema, Query, Resolver } from "type-graphql";
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
+@Resolver()
+class HelloResolver {
+  @Query(() => String)
+  async hello() {
+    return "Hello World";
+  }
+}
 
-app.listen(port, () => {
-  return console.log(`Express is listening at http://localhost:${port}`);
-});
+const main = async () => {
+  const schema = await buildSchema({
+    resolvers: [HelloResolver],
+  });
+  const apolloServer = new ApolloServer({
+    schema,
+    plugins: [ApolloServerPluginLandingPageGraphQLPlayground()],
+  });
+  await apolloServer.start();
+  const app = express();
+  apolloServer.applyMiddleware({ app });
+
+  app.listen(4000, () => {
+    console.log(
+      "Apollo Server up and running at http://localhost:4000/graphql"
+    );
+  });
+};
+
+main();
