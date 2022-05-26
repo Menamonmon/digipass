cc12842347fe3e05e6549ace81eca461b72a91b2
Mena Filfil <mena@Menas-MacBook-Pro.local>
2022-05-26 10:16:37 -0400

    added left & right icon support on button

--- client/components/Button.tsx
+++ client/components/Button.tsx
@@ -3,39 +3,39 @@ import clsx from "clsx";
 import Link from "next/link";
 
 interface ButtonProps extends ButtonHTMLAttributes<{}> {
-  variant: "outlined" | "filled";
+  variant?: "outlined" | "filled";
   href?: string;
+  leftIcon?: React.ComponentElement<any, any>;
+  rightIcon?: React.ComponentElement<any, any>;
 }
 
 const Button: React.FC<ButtonProps> = ({
   className,
   href,
   variant,
+  children,
+  leftIcon,
+  rightIcon,
   ...props
 }) => {
-  return href !== undefined ? (
-    <Link href={href}>
-      <button
-        {...props}
-        className={clsx(
-          className,
-          variant === "outlined"
-            ? "rounded-lg text-primary border-2 border-primary p-2 hover:bg-blue-200 duration-300"
-            : "rounded-lg bg-primary text-secondary p-2 hover:brightness-90 duration-300"
-        )}
-      />
-    </Link>
-  ) : (
+  variant = variant == undefined ? "filled" : variant;
+  const myButton = (
     <button
       {...props}
       className={clsx(
         className,
+        "rounded-lg p-2 duration-300 flex flex-row gap-2 justify-center items-center disabled:brightness-50",
         variant === "outlined"
-          ? "rounded-lg text-primary border-2 border-primary p-2 hover:bg-blue-200 duration-300"
-          : "rounded-lg bg-primary text-secondary p-2 hover:brightness-90 duration-300"
+          ? "text-primary border-2 border-primary hover:bg-blue-200"
+          : "bg-primary text-secondary hover:brightness-90"
       )}
-    />
+    >
+      {leftIcon && <p className="text-2xl">{leftIcon}</p>}
+      {children}
+      {rightIcon && <p className="text-2xl">{rightIcon}</p>}
+    </button>
   );
+  return href !== undefined ? <Link href={href}>{myButton}</Link> : myButton;
 };
 
 export default Button;
