9564650af590dac569d5b72a9436e1517a008e96
Mena Filfil <mena@Menas-MacBook-Pro.local>
2022-05-26 10:17:32 -0400

    added env vars to config to fix their reading

--- client/next.config.js
+++ client/next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  env: {
+    GOOGLE_OAUTH_CLIENT_ID: process.env.GOOGLE_OAUTH_CLIENT_ID,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
