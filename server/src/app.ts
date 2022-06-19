d9241ed94c6a720854d994379aa7da5bf216e072
Mena Filfil <mena@Menas-MacBook-Pro.local>
2022-06-19 17:05:52 -0400

    removed unused imports

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,18 +1,10 @@
-import { authChecker } from "./auth/auth-checker";
-import { ApolloServerPluginLandingPageGraphQLPlayground } from "apollo-server-core/dist/plugin/landingPage/graphqlPlayground";
-import { ApolloServer } from "apollo-server-express";
 import express from "express";
 import "reflect-metadata";
-import { buildSchema } from "type-graphql";
-import { PrismaClient } from "@prisma/client";
 import cors from "cors";
 import cookieParser from "cookie-parser";
-import { authResolvers } from "./auth/resolvers";
 import dotenv from "dotenv";
 import { expressjwt } from "express-jwt";
-import { passesResolvers } from "./passes/resolvers";
 import "./auth/authorizations";
-import { classroomsResolvers } from "./classrooms/resolvers";
 import api from "./api";
 import websockets from "./websockets";
 
