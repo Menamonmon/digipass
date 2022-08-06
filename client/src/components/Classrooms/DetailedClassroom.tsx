1d4cb0da53a0f015d6660e5cdd1d05d4efc91460
Mena F <menaf@Menas-MacBook-Pro.local>
2022-08-05 23:51:49 -0400

    added functionality for archiving classes on frontned

--- client/src/components/Classrooms/DetailedClassroom.tsx
+++ client/src/components/Classrooms/DetailedClassroom.tsx
@@ -1,14 +1,18 @@
 import Link from "next/link";
-import React from "react";
+import React, { useCallback } from "react";
 import { HiClock, HiLockClosed, HiLockOpen } from "react-icons/hi";
 import { MdContentCopy, MdEdit } from "react-icons/md";
+import { useMutation } from "react-relay";
+import { toast } from "react-toastify";
+import { ArchiveClassroomMutation } from "../../graphql/mutations/ArchiveClassroomMutation";
+import { ArchiveClassroomMutation as ArchiveClassroomMutationType } from "../../graphql/mutations/__generated__/ArchiveClassroomMutation.graphql";
 
 interface DetailedClassroomProps {
   id: string;
   title: string;
   description?: string | null;
   classCode: string;
-  createdAt: Date;
+  createdAt: string;
   archived: boolean;
   startHour: number;
   startMinute: number;
@@ -28,16 +32,62 @@ export const DetailedClassroom: React.FC<DetailedClassroomProps> = ({
   startMinute,
   endMinute,
 }) => {
-  const archive = () => {};
+  const [commitClassArchive, isArchivingClass] =
+    useMutation<ArchiveClassroomMutationType>(ArchiveClassroomMutation);
+  const archive = () => {
+    commitClassArchive({
+      variables: { classroomId: id },
+      onCompleted(response, errors) {
+        if (response.archiveClassroom) {
+          toast("Successfully archived class", {
+            autoClose: 1000,
+            type: "success",
+          });
+        }
+
+        if (errors) {
+          toast(
+            "An error occurred while trying to archive the classroom. Try again later",
+            {
+              autoClose: 2000,
+              type: "error",
+            }
+          );
+        }
+      },
+    });
+  };
+
+  const notifyOfSuccessfulCopy = () =>
+    toast("Copied", {
+      autoClose: 500,
+      type: "success",
+      pauseOnFocusLoss: false,
+      pauseOnHover: false,
+    });
+  const copyClassCodeToClipboard = useCallback(async () => {
+    navigator.clipboard.writeText(classCode).then(notifyOfSuccessfulCopy);
+  }, [classCode]);
+
+  const createdAtDate = new Date(createdAt);
+  const formattedCreatedAtDate = `${
+    createdAtDate.getMonth() + 1
+  }/${createdAtDate.getDate()}/${createdAtDate.getFullYear()}`;
+
   return (
-    <div className="p-3 rounded-lg w-96 bg-secondary">
+    <div className="p-3 rounded-lg h-50 w-96 bg-secondary">
       <div className="flex items-center justify-between text-primary">
-        <h4 className="inline">{title}</h4>
+        <div className="flex items-center justify-center gap-2">
+          <Link href={`/teacher/classrooms/${id}`}>
+            <h4 className="hover:link">{title}</h4>
+          </Link>
+          <p className="text-gray-500">({formattedCreatedAtDate})</p>
+        </div>
         {!archived ? (
           <button
             className="flex justify-center text-2xl btn btn-circle btn-sm tooltip"
             onClick={archive}
-            data-tip="Click to archive"
+            data-tip="Archive class"
           >
             <HiLockOpen />
           </button>
@@ -47,12 +97,7 @@ export const DetailedClassroom: React.FC<DetailedClassroomProps> = ({
           </div>
         )}
       </div>
-      <p className="text-gray-800 line-clamp-4">
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatibus
-        doloremque, vel commodi quidem officiis eos deserunt ex tenetur sapiente
-        quos aliquid quibusdam quam ad ipsam assumenda praesentium animi vitae!
-        Rerum!
-      </p>
+      <p className="text-gray-800 line-clamp-4">{description}</p>
       {/* Footer */}
       <div className="flex justify-between mt-2">
         <div className="inline-flex items-center gap-2">
@@ -68,12 +113,15 @@ export const DetailedClassroom: React.FC<DetailedClassroomProps> = ({
           </div>
         </div>
         <div className="flex gap-2">
-          <button className="text-xl btn btn-circle btn-sm">
-            <MdEdit />
-          </button>
+          <Link href={`/teacher/classrooms/${id}/edit`}>
+            <button className="text-xl btn btn-circle btn-sm">
+              <MdEdit />
+            </button>
+          </Link>
           <button
             className="flex items-center text-xl btn btn-circle btn-sm tooltip"
             data-tip="Copy class code"
+            onClick={copyClassCodeToClipboard}
           >
             <MdContentCopy />
           </button>
--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -1,10 +1,12 @@
 import "../styles/globals.css";
+import "react-toastify/dist/ReactToastify.css";
 import type { AppProps } from "next/app";
 import { AuthContextProvider } from "../hooks/useAuth";
 import { RelayEnvironmentProvider } from "react-relay";
 import { RelayEnvironment } from "../graphql/client";
 import { useAppLayout } from "../hooks/useAppLayout";
 import { Layout as NavbarLayout } from "../components/Layout";
+import { ToastContainer } from "react-toastify";
 
 function MyApp({ Component, pageProps }: AppProps) {
   const AppLayout = useAppLayout();
@@ -12,6 +14,7 @@ function MyApp({ Component, pageProps }: AppProps) {
     <div data-theme="mytheme">
       <RelayEnvironmentProvider environment={RelayEnvironment}>
         <AuthContextProvider>
+          <ToastContainer />
           <AppLayout>
             <NavbarLayout>
               <Component {...pageProps} />
