696df786b84dcca14dad59e7459fa97bf1bb0822
Mena <58854232+Menamonmon@users.noreply.github.com>
2022-11-05 18:45:42 -0400

    cleanup

--- client/src/components/Classrooms/ClassroomTabs/StudentsSearchBar.tsx
+++
@@ -1,8 +0,0 @@
-import { match } from "assert";
-import React from "react";
-
-const StudentsSearchBar: React.FC = () => {
-  return <div>Students Search Bar</div>;
-};
-
-export default StudentsSearchBar;
