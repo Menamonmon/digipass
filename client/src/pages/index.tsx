c0001e901a41d8eafe591db13a42ae8039f14dd6
Mena <58854232+Menamonmon@users.noreply.github.com>
2022-11-24 15:58:14 -0500

    added option for user to choose which type of account they login with

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,15 +1,23 @@
 import type { NextPage } from "next";
 import Head from "next/head";
 import { Layout } from "../components/Layout";
+import useAuth from "../hooks/useAuth";
 
 const Home: NextPage = () => {
+  const { authStatus } = useAuth();
   return (
     <>
       <Head>
         <title>Welcome to digipass!</title>
       </Head>
       <div>
-        <a href="teacher/classrooms">Classrooms</a>
+        <a
+          href={`${
+            authStatus.endsWith("student") ? "student" : "teacher"
+          }/classrooms`}
+        >
+          Classrooms
+        </a>
       </div>
     </>
   );
