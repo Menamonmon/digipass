9cd289cd254b33ee80186744dfceab60738fbb39
Mena <58854232+Menamonmon@users.noreply.github.com>
2023-08-04 01:22:21 -0700

    Removed unused packages

--- client/src/hooks/useSockets.ts
+++ client/src/hooks/useSockets.ts
@@ -1,55 +1,55 @@
-import { CurrentUserJwtInfo } from "./../../../server/common/types";
-import { config } from "./../../config";
-import { useState } from "react";
-import io, { Socket } from "socket.io-client";
-import useAuth from "./useAuth";
+// import { CurrentUserJwtInfo } from "./../../../server/common/types";
+// import { config } from "./../../config";
+// import { useState } from "react";
+// import io, { Socket } from "socket.io-client";
+// import useAuth from "./useAuth";
 
-interface ExpandedSocket extends Socket {
-  userInfo: CurrentUserJwtInfo;
-}
+// interface ExpandedSocket extends Socket {
+//   userInfo: CurrentUserJwtInfo;
+// }
 
-export const useSockets = async () => {
-  const { authStatus, jwt } = useAuth();
-  const [socket, setSocket] = useState<ExpandedSocket>();
+// export const useSockets = async () => {
+//   const { authStatus, jwt } = useAuth();
+//   const [socket, setSocket] = useState<ExpandedSocket>();
 
-  const initializeSocket = async () => {
-    const newSocket = io(config.backendUrl, {
-      query: { jwt },
-    }) as ExpandedSocket;
-    setSocket(newSocket);
+//   const initializeSocket = async () => {
+//     const newSocket = io(config.backendUrl, {
+//       query: { jwt },
+//     }) as ExpandedSocket;
+//     setSocket(newSocket);
 
-    if (socket) {
-      socket.on("connect", () => {
-        console.log("connected");
-      });
-    }
-  };
+//     if (socket) {
+//       socket.on("connect", () => {
+//         console.log("connected");
+//       });
+//     }
+//   };
 
-  const initiateTeacherSesssion = () => {
-    if (authStatus.endsWith("teacher") && socket) {
-      socket.emit("placement");
-    } else {
-      alert("ERROR: PLACEMENT FOR TEACHER FAILED");
-    }
-  };
-  const placeStudentInClassroom = ({
-    classroomId,
-    teacherId,
-  }: {
-    classroomId: string;
-    teacherId: string;
-  }) => {
-    if (authStatus.endsWith("student") && socket) {
-      socket.emit("placement", { classroomId, teacherId });
-    } else {
-      alert("ERROR: PLACEMENT FOR STUDENT FAILED");
-    }
-  };
+//   const initiateTeacherSesssion = () => {
+//     if (authStatus.endsWith("teacher") && socket) {
+//       socket.emit("placement");
+//     } else {
+//       alert("ERROR: PLACEMENT FOR TEACHER FAILED");
+//     }
+//   };
+//   const placeStudentInClassroom = ({
+//     classroomId,
+//     teacherId,
+//   }: {
+//     classroomId: string;
+//     teacherId: string;
+//   }) => {
+//     if (authStatus.endsWith("student") && socket) {
+//       socket.emit("placement", { classroomId, teacherId });
+//     } else {
+//       alert("ERROR: PLACEMENT FOR STUDENT FAILED");
+//     }
+//   };
 
-  return {
-    socket,
-    initializeSocket,
-    initiateTeacherSesssion,
-    placeStudentInClassroom,
-  };
-};
+//   return {
+//     socket,
+//     initializeSocket,
+//     initiateTeacherSesssion,
+//     placeStudentInClassroom,
+//   };
+// };
